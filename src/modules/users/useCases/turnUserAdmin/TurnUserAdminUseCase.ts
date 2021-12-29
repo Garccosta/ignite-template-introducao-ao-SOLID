0282c2c4c70427931305d4dbb66358572cbd27d8
“gabriel.alcantara” <gabriel.alcantara@g.globo>
2021-12-29 00:02:01 -0300

    Finished turnUserAdmin use case

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,14 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("There is no user with this id!");
+    }
+
+    this.usersRepository.turnAdmin(user);
+    return user;
   }
 }
 
